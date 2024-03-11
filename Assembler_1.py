80d9f80bf5459842b8e3876dd6fdc53b80ca8ec0
smilieshreya <162793807+smilieshreya@users.noreply.github.com>
2024-03-11 23:06:51 +0530

    Update Assembler_1.py

--- Assembler_1.py
+++ Assembler_1.py
@@ -20,3 +20,23 @@ def bitbinary12(decimal):
     else:
         binary = bin(2**12 + decimal)[2:]
     return binary
+
+def instruc_type(x):
+    """function to check what is the type of instruction """
+    #R = 1; I = 2; S = 3; B = 4; U = 5; J = 6
+    type_ = 9
+    if x in r_opcodes:
+        type_ = 1
+    elif x in i_opcodes:
+        type_ = 2
+    elif x in s_opcodes:
+        type_ = 3
+    elif x in b_opcodes:
+        type_ = 4
+    elif x in u_opcodes:
+        type_ = 5
+    elif x in j_opcodes:
+        type_ = 6
+    elif x not in r_opcodes or i_opcodes or s_opcodes or b_opcodes or u_opcodes or j_opcodes:
+        type_= 7     
+    return type_
