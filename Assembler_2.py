661d99f160b04b7a363cb73de0e5e9531927c6f0
Ojas Kumar <162628924+OJASx@users.noreply.github.com>
2024-03-12 13:48:11 +0530

    Update Assembler_2.py

--- Assembler_2.py
+++ Assembler_2.py
@@ -382,11 +382,13 @@ temp_binary_list = [
 "bltu"
 "bgeu"
 "lui"
-]
+] 
 
+in_ = input("Enter the Input File Path: ")
+out = input("Enter the Output File Path")
 
-pointer = open("<file input path>","r")
-w_pointer = open("<file input path>","w")
+pointer = open(in_,"r")
+w_pointer = open(out,"w")
 s = pointer.readlines()
 #print(len(s))
 pointer.seek(0)
